7a935b20f747dae42ecc2a34e5999cd71c01f2ab
kennyquast <kenny@quast.ca>
2019-03-02 07:21:56 -0500

    Add project files.

---
+++ 352ShippingLabelMakerUpdater/Module1.vb
@@ -0,0 +1,180 @@
+﻿Imports System
+Imports System.IO
+Module Module1
+    Public HomeFolder As String = ""
+    Public DownloadFolder As String = ""
+    Public VersionInfo As String = ""
+    Public AppPath As String = My.Application.Info.DirectoryPath
+    Sub Main(ByVal args() As String)
+        Dim versionNumber As Version
+
+        versionNumber = My.Application.Info.Version
+        Console.Write("352ShippingLabelMaker Updater file version : ")
+        Console.WriteLine(versionNumber)
+        Console.WriteLine(vbLf)
+        If args.Length = 0 Then
+            Console.WriteLine("Error: No home folder specified")
+            Console.WriteLine("This program must be run from the main application Or use the following format.")
+            Console.WriteLine("352ShipingLabelMakerUpdater.exe homefolderpath   - eg: C:\homefolder")
+            Console.WriteLine("Press Any Key to Exit...")
+            Console.ReadLine()
+            End
+        Else
+            Dim data As String = args(0)
+            HomeFolder = data
+        End If
+
+
+
+        ' Console.WriteLine("Argument: {0}", data)
+        ' Console.Write("Press Enter to exit")
+        ' Console.Read()
+
+        'uncomment all below for full functioality
+        Kill352ShippingLabelMaker()
+        Console.WriteLine("Pausing for 3 seconds to close Label Maker Software Please Wait...")
+        '3 second delay should be enough to kill this 
+        'process as the application SHOULD be closed before this is launched anyway
+        Threading.Thread.Sleep(3000)
+        BackupFiles()
+        GetCurrentVersion()        '
+        'Download Files from server
+        CopyDirectory(HomeFolder & "\updates\" & VersionInfo, AppPath, True)
+        'restart label scanner
+        Shell("352ShippingLabelMaker.exe")
+        End
+    End Sub
+    Public Sub Kill352ShippingLabelMaker()
+
+        For Each prog As Process In Process.GetProcesses
+            If prog.ProcessName = "352ShippingLabelMaker" Then
+                prog.Kill()
+            End If
+        Next
+        Exit Sub
+
+    End Sub
+    Public Sub BackupFiles()
+        'Delete the oldest backup from last time
+        Dim bk As String = "working\352ShippingLabelMaker.exe"
+        File.Delete(bk)
+        'copy the current version into a workign folder
+        Dim file1 As String = "352ShippingLabelMaker.exe"
+        Dim file2 As String = "working\352shippinglabelmaker.exe"
+        File.Copy(file1, file2)
+
+
+
+
+    End Sub
+    Private Sub GetCurrentVersion()
+
+        'read a file that contains the most current version info dfrom the server
+        Dim lines() As String = IO.File.ReadAllLines(HomeFolder & "\currentversion.txt")
+        VersionInfo = lines(0)
+
+    End Sub
+
+    Sub CopyDirectory(ByVal SourcePath As String, ByVal DestPath As String, Optional ByVal Overwrite As Boolean = False)
+
+        Dim SourceDir As DirectoryInfo = New DirectoryInfo(SourcePath)
+
+        Dim DestDir As DirectoryInfo = New DirectoryInfo(DestPath)
+
+
+
+        ' the source directory must exist, otherwise throw an exception
+
+        If SourceDir.Exists Then
+
+            ' if destination SubDir's parent SubDir does not exist throw an exception
+
+            If Not DestDir.Parent.Exists Then
+
+                Throw New DirectoryNotFoundException("Destination directory does not exist: " + DestDir.Parent.FullName)
+
+            End If
+
+
+
+            If Not DestDir.Exists Then
+
+                DestDir.Create()
+
+            End If
+
+
+
+            ' copy all the files of the current directory
+
+            Dim ChildFile As FileInfo
+
+            For Each ChildFile In SourceDir.GetFiles()
+
+                If Overwrite Then
+
+                    ChildFile.CopyTo(Path.Combine(DestDir.FullName, ChildFile.Name), True)
+
+                Else
+
+                    ' if Overwrite = false, copy the file only if it does not exist
+
+                    ' this is done to avoid an IOException if a file already exists
+
+                    ' this way the other files can be copied anyway...
+
+                    If Not File.Exists(Path.Combine(DestDir.FullName, ChildFile.Name)) Then
+
+                        ChildFile.CopyTo(Path.Combine(DestDir.FullName, ChildFile.Name), False)
+
+                    End If
+
+                End If
+
+            Next
+
+
+
+            ' copy all the sub-directories by recursively calling this same routine
+
+            Dim SubDir As DirectoryInfo
+
+            For Each SubDir In SourceDir.GetDirectories()
+
+                CopyDirectory(SubDir.FullName, Path.Combine(DestDir.FullName, SubDir.Name), Overwrite)
+
+            Next
+
+        Else
+
+            Throw New DirectoryNotFoundException("Source directory does not exist: " + SourceDir.FullName)
+
+        End If
+
+    End Sub
+    Public Sub MakeFolder()
+        'Make Folder not needed, but code left for educational purposes
+        ' Specify the directories you want to manipulate.
+        Dim di As DirectoryInfo = New DirectoryInfo("test")
+        Try
+            ' Determine whether the directory exists.
+            If di.Exists Then
+                ' Indicate that it already exists.
+                Console.WriteLine("That path exists already.")
+                Return
+            End If
+
+            ' Try to create the directory.
+            di.Create()
+            Console.WriteLine("The directory was created successfully.")
+
+            '' Delete the directory.
+            'di.Delete()
+            'Console.WriteLine("The directory was deleted successfully.")
+
+        Catch e As Exception
+            Console.WriteLine("The process failed: {0}", e.ToString())
+        End Try
+    End Sub
+
+End Module
---
+++ 352ShippingLabelMakerUpdater/My Project/AssemblyInfo.vb
@@ -0,0 +1,35 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' General Information about an assembly is controlled through the following
+' set of attributes. Change these attribute values to modify the information
+' associated with an assembly.
+
+' Review the values of the assembly attributes
+
+<Assembly: AssemblyTitle("352ShippingLabelMakerUpdater")>
+<Assembly: AssemblyDescription("updater for 352ShippingLabelMaker")>
+<Assembly: AssemblyCompany("Tiercon Corp")>
+<Assembly: AssemblyProduct("352ShippingLabelMakerUpdater")>
+<Assembly: AssemblyCopyright("Copyright © 2018")>
+<Assembly: AssemblyTrademark("")>
+
+<Assembly: ComVisible(False)>
+
+'The following GUID is for the ID of the typelib if this project is exposed to COM
+<Assembly: Guid("a15d50ea-f489-4658-b062-c4413bd9efcf")>
+
+' Version information for an assembly consists of the following four values:
+'
+'      Major Version
+'      Minor Version
+'      Build Number
+'      Revision
+'
+' You can specify all the values or you can default the Build and Revision Numbers
+' by using the '*' as shown below:
+' <Assembly: AssemblyVersion("1.0.*")>
+
+<Assembly: AssemblyVersion("1.1.0.0")>
+<Assembly: AssemblyFileVersion("1.1.0.0")>
